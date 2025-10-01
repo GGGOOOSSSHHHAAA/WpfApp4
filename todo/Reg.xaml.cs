6901f7272d1f55e0f263b82abf9dfea62312627d
GGGOOOSSSHHHAAA <vilka.sk8t@gmail.com>
2025-10-01 20:51:55 +0300

    Добавьте файлы проекта.

---
+++ todo/Reg.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Entities;
+using todo.Repository;
+
+namespace todo
+{
+    /// <summary>
+    /// Логика взаимодействия для Reg.xaml
+    /// </summary>
+    public partial class Reg : Window
+    {
+        public Reg()
+        {
+            InitializeComponent();
+        }
+
+        private void regbut2_Click(object sender, RoutedEventArgs e)
+        { 
+            var name = name_textbox.Text;
+            var email = email_textbox.Text;
+            var pass1 = pass1_textbox.Text;
+            var pass2 = pass2_textbox.Text;
+
+           
+
+            try
+            {
+                UserRepository.GetInstance().Register(new Entities.UserModel() { Email = email, Name = name, Pass = pass1 }, pass2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            new MainEmpty().Show();
+            Close();
+        }
+
+        private void backbut1_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            Close();
+        }
+    }
+}
